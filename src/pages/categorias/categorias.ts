0585baa5df998792fc1609c2b7665f3279b5a29b
Diego Munhoz <diego.fatec@hotmail.com>
2019-12-03 09:38:38 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -26,8 +26,6 @@ export class CategoriasPage {
       .subscribe(response => {
         this.items = response
       },
-      error => {
-        console.log(error)
-      })
+      error => {})
   }
 }
