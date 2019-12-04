cac8cb4153a7f672674344d509dc6ba6c8324753
Diego Munhoz <diego.fatec@hotmail.com>
2019-12-04 14:07:30 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(public storageService: StorageService) {
+  constructor(public storageService: StorageService, public alertCtrl: AlertController) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -26,8 +27,16 @@ export class ErrorInterceptor implements HttpInterceptor {
           console.log(errorObj)
 
           switch (errorObj.status) {
+            case 401:
+              this.handle401()
+              break
+
             case 403:
               this.handle403()
+              break
+
+            default:
+              this.handleDefaultError(errorObj)
               break
           }
 
@@ -35,8 +44,36 @@ export class ErrorInterceptor implements HttpInterceptor {
       }) as any
   }
 
+  handle401() {
+    let alert = this.alertCtrl.create({
+      title: 'Erro 401, falha de autenticação',
+      message: 'Email ou senha incorretos!',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok'
+        }
+      ]
+    })
+    alert.present()
+  }
+
   handle403() {
     this.storageService.setLocalUser(null)
+  }
+
+  handleDefaultError(errorObj) {
+    let alert = this.alertCtrl.create({
+      title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok'
+        }
+      ]
+    })
+    alert.present()
   }
 
 }
