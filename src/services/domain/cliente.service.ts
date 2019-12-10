321976d5b5b7a96502b37fce40f526ed1a1e884b
Diego Munhoz <diego.fatec@hotmail.com>
2019-12-10 10:08:53 -0300

    Buscando enderecos do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -13,8 +13,8 @@ export class ClienteService {
     public storageService: StorageService){
   }
 
-  findByEmail(email: string): Observable<ClienteDTO> {
-    return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`)
+  findByEmail(email: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`)
   }
 
   getImageFromBucket(id: string): Observable<any> {
