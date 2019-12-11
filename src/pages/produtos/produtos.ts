c41802989c540a9c225b2373a0ee99eee910dac3
Diego Munhoz <diego.fatec@hotmail.com>
2019-12-11 10:12:25 -0300

    Implementando componente Infinte scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,9 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = []
+
+  page: number = 0
 
   constructor(
     public navCtrl: NavController,
@@ -27,10 +29,14 @@ export class ProdutosPage {
   loadData() {
     let categoria_id = this.navParams.get('categoria_id')
     let loader = this.presentLoading()
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content']
-        this.loadImageUrls()
+        let start = this.items.length
+        this.items = this.items.concat(response['content'])
+        let end= this.items.length - 1
+        //console.log(this.page)
+        //console.log(this.items);
+        this.loadImageUrls(start, end)
         loader.dismiss()
       },
       error => {
@@ -38,8 +44,8 @@ export class ProdutosPage {
       })
   }
 
-  loadImageUrls() {
-    for (var i=0; i < this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i=start; i <= end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -62,9 +68,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = []
     this.loadData();
     setTimeout(() => {
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 
